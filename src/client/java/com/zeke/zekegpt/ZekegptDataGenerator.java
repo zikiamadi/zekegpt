6992176744aee7355959a90940ea3c43403bac32
zikiamadi-a11y <zikiamadi@gmail.com>
2026-03-16 12:08:05 -0700

    Initial commit

---
+++ src/client/java/com/zeke/zekegpt/ZekegptDataGenerator.java
@@ -0,0 +1,11 @@
+package com.zeke.zekegpt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZekegptDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
